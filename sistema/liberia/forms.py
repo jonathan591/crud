5c360a86c320980aba6a4966ef7a1961af42dc44
RoxanaB01 <roxanabarzola45@gmail.com>
2023-08-31 17:11:24 -0500

    first commit

---
+++ sistema/liberia/forms.py
@@ -0,0 +1,11 @@
+from dataclasses import field
+from pyexpat import model
+from django import forms
+from .models import Libro
+
+class LibroForm(forms.ModelForm):
+    class Meta:
+        model=Libro
+        fields='__all__'
+
+            
